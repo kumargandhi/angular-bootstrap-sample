480acab3b6df37ae619d5411bbfc1af6d20e4f99
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-01-04 13:22:28 +0530

    Form page sample

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,7 +4,13 @@ import {
     Component,
     OnInit,
 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+    FormArray,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     selector: 'app-form',
@@ -17,11 +23,80 @@ export class FormComponent implements OnInit {
 
     isFormSubmitted = false;
 
+    selectedOption = 0;
+
+    isOptionSelected = false;
+
     constructor(private _fb: FormBuilder, private _cd: ChangeDetectorRef) {
         this.form = this._fb.group({
             question: ['', Validators.required],
+            options: this._fb.array([]),
         });
     }
 
+    get optionsFormArray(): FormArray {
+        return this.form.get('options') as FormArray;
+    }
+
     ngOnInit(): void {}
+
+    optionClicked(index: number) {
+        this.selectedOption = index;
+        this.isOptionSelected = true;
+    }
+
+    reset() {}
+
+    addOption() {
+        const options = this.form.get('options') as FormArray;
+        const option = new FormGroup({
+            selected: new FormControl(),
+            answer: new FormControl('', [Validators.required]),
+        });
+        options.push(option);
+    }
+
+    submit() {
+        this.isFormSubmitted = true;
+    }
+
+    setDefault() {
+        const { question, options } = this.form.controls;
+        question.setValue('Which of the following numbers is farthest from the number 1 on the number line?');
+        const answers: string[] = ['0', '-5', '-10', '5', '10'];
+        const selectedOption = this.selectedOption = 2;
+        this.isOptionSelected = true;
+        (options as FormArray).clear();
+        answers.forEach((item) => {
+            const option = new FormGroup({
+                selected: new FormControl(),
+                answer: new FormControl(item, [Validators.required]),
+            });
+            (options as FormArray).push(option);
+        });
+        (options as FormArray).controls[selectedOption].get('selected')?.setValue('selected' + selectedOption);
+    }
+
+    getOptionsFormGroup(index: number): FormGroup {
+        const options = this.form.get('options') as FormArray;
+        return options.get('' + index) as FormGroup;
+    }
+
+    isAddAnswerBtnDisabled() {
+        if (!this.isOptionSelected) {
+            return true;
+        }
+        const options = this.form.get('answers') as FormArray;
+        let isValid = true;
+        for (let i = 0; i < options.controls.length; i++) {
+            const fg = options.get('' + i) as FormGroup;
+            if (fg.controls['answer'].touched && !fg.controls['answer'].valid) {
+                isValid = !fg.controls['answer'].errors?.required;
+            }
+            if (!isValid) {
+                break;
+            }
+        }
+        return !isValid;
+    }
 }
