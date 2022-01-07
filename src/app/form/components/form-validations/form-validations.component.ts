fd3ae4c65d2eebc2e03bb822437a488f1e3ad994
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-01-07 21:04:32 +0530

    Form validation changes

--- src/app/form/components/form-validations/form-validations.component.ts
+++ src/app/form/components/form-validations/form-validations.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'app-form-validations',
     templateUrl: './form-validations.component.html',
     styleUrls: ['./form-validations.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormValidationsComponent implements OnInit {
-    constructor() {
-        // do nothing
+    form: FormGroup;
+
+    isFormSubmitted = false;
+
+    formDesc: string;
+
+    constructor(private _fb: FormBuilder, private _cd: ChangeDetectorRef) {
+        this.form = this._fb.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            username: ['', Validators.required],
+            city: ['', Validators.required],
+            state: ['', Validators.required],
+            zip: ['', Validators.required],
+            toc: [false],
+        });
     }
 
     ngOnInit(): void {
         // do nothing
+    }
+
+    onFormSubmit($event: any) {
+        if (!$event) {
+            return;
+        }
+        this.isFormSubmitted = true;
+        this.formDesc = '';
+        _.forIn(this.form.controls, (value, key) => {
+            this.formDesc += `Control ${key} with value ${value.value}<br>`;
+        });
     }
 }
